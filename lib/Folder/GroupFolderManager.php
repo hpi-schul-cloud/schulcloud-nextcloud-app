ec3ae0df066a88d67663aa0328dfff56c6d29c83
agnisa-cap <arne.gnisa@capgemini.com>
2022-11-16 11:17:20 +0100

    WIP

--- lib/Folder/GroupFolderManager.php
+++ lib/Folder/GroupFolderManager.php
@@ -1,39 +1,64 @@
 <?php
 namespace OCA\Schulcloud\Folder;
 
+use OCP\IGroup;
+use OCP\IGroupManager;
 use OCP\ILogger;
 
 use OCA\GroupFolders\Folder\FolderManager;
 
-use OCA\Schulcloud\Helper\GroupFolderName;
+use OCA\Schulcloud\Helper\GroupFolder;
 
 class GroupFolderManager {
 
     /** @var FolderManager from Group Folders*/
-    private $folderManager;
+    private FolderManager $folderManager;
 
     /** @var ILogger */
-    private $logger;
+    private ILogger $logger;
 
-    public function __construct(FolderManager $folderManager, ILogger $logger) {
+    /** @var IGroupManager */
+    private IGroupManager $groupManager;
+
+    public function __construct(FolderManager $folderManager, ILogger $logger, IGroupManager $groupManager = null) {
         $this->folderManager = $folderManager;
         $this->logger = $logger;
+        if (!$groupManager) {
+            $groupManager = \OC::$server->get(IGroupManager::class);
+        }
+        $this->groupManager = $groupManager;
     }
 
     /**
-     * Creates a group folder and associates a group with it
-     * @param $group
+     * Creates a group folder and associates a group with it.
+     * SchulcloudNextcloud-637204050b71c976b8f6425e_teamowner
+     * @param $group { "gid": "SchulcloudNextcloud-5e1dba1eaa30ab4df47e11d2_teamowner", "displayName": "Test Team" }
      * @return void
      */
     public function createFolderForGroup($group) {
         $groupId = $group->getGID();
-        $folderName = GroupFolderName::make($groupId, $group->getDisplayName());
+        $folderName = GroupFolder::make($groupId, $group->getDisplayName());
+
+        $this->logger->warning("gid: ".$groupId);
+        $foundGroups = $this->groupManager->search(GroupFolder::extractFolderId($groupId));
+        $this->logger->warning(json_encode(array_values($foundGroups)));
+
 
         try {
-            $folderId = $this->folderManager->createFolder($folderName);
+            $this->logger->warning("count of foundgroups ".count($foundGroups));
+            if (count($foundGroups) > 0) {
+                if (count($foundGroups) > 1) {
+                    $this->logger->error("To many groupfolders were found for the same groupId $groupId");
+                }
+                $folder = $this->folderManager->getFoldersForGroup($foundGroups[0]->getGID())[0];
+                $folderId = $folder['folder_id'];
+            } else {
+                $folderId = $this->folderManager->createFolder($folderName);
+                $this->logger->warning("Successfully created new group folder: [id: $folderId, name: $folderName]");
+            }
             $this->folderManager->addApplicableGroup($folderId, $groupId);
+            $this->logger->warning("Successfully added group $groupId to: [id: $folderId, name: $folderName]");
 
-            $this->logger->info("Successfully created new group folder: [id: $folderId, name: $folderName]");
         } catch(\Exception $e) {
             $this->logger->error("Failed to create group folder:\n" . $e->getMessage());
         }
@@ -50,12 +75,12 @@ class GroupFolderManager {
 
             foreach ($folderList as $folder) {
                 $folderId = $folder['folder_id'];
-                
+
                 $groups = $this->folderManager->searchGroups($folderId);
                 // Assumes that the automatically generated group folders are named after the first (and only) connected group
                 $group = $groups[0];
-                
-                $newFolderName = GroupFolderName::make($group['gid'], $group['displayname']);
+
+                $newFolderName = GroupFolder::make($group['gid'], $group['displayname']);
 
                 $this->folderManager->renameFolder($folderId, $newFolderName);
             }
--- lib/Helper/GroupFolder.php
+++ lib/Helper/GroupFolder.php
@@ -1,7 +1,7 @@
 <?php
 namespace OCA\Schulcloud\Helper;
 
-class GroupFolderName {
+class GroupFolder {
 
     /**
      * Creates a formatted string for the group folder names from the gid and display name
@@ -13,9 +13,18 @@ class GroupFolderName {
         $id = $groupId;
 
         if (preg_match("/(?<=-)(.*)$/", $groupId, $matches)) {
-            $id = $matches[0];
+            $id = GroupFolder::extractFolderId($matches[0]);
         }
-        
+
         return "$groupName ($id)";
+    }
+
+    /**
+     * Extracts the folderId out of the gid by splitting on underscore to remove the role of the user.
+     * @param $gid "637204050b71c976b8f6425e_teamowner"
+     * @return string
+     */
+    public static function extractFolderId($gid) {
+        return explode('_', $gid, 2)[0];
     }
 }
